647e5a59b7ce48602ab68ca9eae773a4ee9ee492
matiasBeroisa <mberoisa9@gmail.com>
2025-04-26 01:11:59 -0300

    parcial completo

--- Haskell/parcial.hs
+++ Haskell/parcial.hs
@@ -1,22 +1,20 @@
-tparcial :: (Int, Int) -> Int -> Int
+tparcial :: (Int, Int) -> Int -> String
 tparcial (n, m) c
   | n <= m && n >= 1 && c > 0 = concatenar (n, m) c
-  | otherwise = 0
+  | otherwise = ""
 
-concatenar :: (Int, Int) -> Int -> Int
+concatenar :: (Int, Int) -> Int -> String
 concatenar (n, m) c
-  | tope >= limite = show n ++ concatenarDigito n (i + 1)
+  | n <= m = concatenar (n, m - 1) c ++ show m ++ "     " ++ show (sumatoria m c) ++ " "
+  | otherwise = ""
+
+sumatoria :: Int -> Int -> Float
+sumatoria k c
+  | k > 0 = fromIntegral (div divisor dividendo) + sumatoria (k - 1) c
   | otherwise = 0
   where
-    tope = m
-    limite = n
-    sumador = sumatoria n m c
-
-sumatoria :: Int -> Int -> Int -> Int
-sumatoria n m c = divisor / dividendo
-  where
-    divisor = potenciaTriple n + (5 * c)
-    dividendo = (n * 2) + (c - 1)
+    divisor = fromIntegral (potenciaTriple k + (5 * c))
+    dividendo = fromIntegral ((k * 2) + (c - 1))
 
 potenciaTriple :: Int -> Int
 potenciaTriple x = x * x * x
